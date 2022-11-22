c281f990205e38f581b2533bff600e3c0dc9da18
dwarf <fd@arista.com>
2022-11-22 16:31:07 +0100

    add the image list and selection

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import xmltodict
 import pprint
 from pydantic import BaseModel
 from jinja2 import FileSystemLoader, Environment
+import docker
+from simple_term_menu import TerminalMenu
 
 
 def readxml(file_name):
@@ -31,7 +33,7 @@ def find_interfaces(links):
     links_tempo = []
     for node_id in links:
         interfaces = [[item.x, item.value] for item in objects_drawio_in_class_node if item.value.lower().find('eth') == 0 and item.parent == node_id[0]]
-        links_tempo.append([f"{find_value(node_id[1])[0]}:{interfaces[0][1]}", f"{find_value(node_id[2])[0]}:{interfaces[1][1]}"])
+        links_tempo.append([f"{find_value(node_id[1])[0]}:{interfaces[0][1].lower()}", f"{find_value(node_id[2])[0]}:{interfaces[1][1].lower()}"])
     return links_tempo
 
 def moving_x(objects_drawio):
@@ -44,7 +46,15 @@ def moving_x(objects_drawio):
 
 
 if __name__ == '__main__':
-    result = readxml("test-drawio-V6")
+    lab_name = input('Enter your Lab name : ')
+    client = docker.from_env()
+    images_docker_brut = client.images.list()
+    images_docker = [str(item).split("'")[1] for item in images_docker_brut]
+    terminal_menu = TerminalMenu(images_docker)
+    menu_entry_index = terminal_menu.show()
+    lab_image = images_docker[menu_entry_index]
+
+    result = readxml("test-drawio-V7")
     my_dict = xmltodict.parse(result)
     objects_drawio = my_dict['mxfile']['diagram']['mxGraphModel']['root']['mxCell']
 
@@ -54,8 +64,8 @@ if __name__ == '__main__':
     links = find_link(objects_drawio_in_class_node)
     links_yaml = find_interfaces(links)
 
-    lab_name = "Lab_test"
-    lab_image = "arista/ceos:4.29.0.2F"
+    # lab_name = "Lab_test"
+    # lab_image = "arista/ceos:4.29.0.2F"
 
     file_loader = FileSystemLoader('templates')
     env = Environment(loader=file_loader)
@@ -63,5 +73,5 @@ if __name__ == '__main__':
 
     output = template.render(lab_name=lab_name,lab_image=lab_image,nodes_yaml=nodes_yaml,links_yaml=links_yaml)
 
-    with open('data/lab.yml', 'w') as yaml_file:
+    with open(f'data/{lab_name}.yml', 'w') as yaml_file:
         yaml_file.write(output)
