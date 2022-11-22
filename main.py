b6b12ae252ad758fc5cfe4745a98b4286ca8d9cb
dwarf <fd@arista.com>
2022-11-22 13:01:50 +0100

    code refactorisation Jinja2 used

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import xmltodict
 import pprint
 from pydantic import BaseModel
-import yaml
+from jinja2 import FileSystemLoader, Environment
 
 
 def readxml(file_name):
@@ -20,49 +20,48 @@ class Node(BaseModel):
 
 
 def find_value(endpoint):
-    return [item.value for item in result_nodes if item.id == endpoint]
+    return [item.value for item in objects_drawio_in_class_node if item.id == endpoint]
+
+
+def find_link(objects_drawio):
+    return [[item.id,item.source, item.target] for item in objects_drawio if item.value == '']
+
+
+def find_interfaces(links):
+    links_tempo = []
+    for node_id in links:
+        interfaces = [[item.x, item.value] for item in objects_drawio_in_class_node if item.value.find('eth') == 0 and item.parent == node_id[0]]
+        links_tempo.append([f"{find_value(node_id[1])[0]}:{interfaces[0][1]}", f"{find_value(node_id[2])[0]}:{interfaces[1][1]}"])
+    return links_tempo
+
+def moving_x(objects_drawio):
+    objects_drawio_without_special_caract = []
+    for item in objects_drawio:
+        if 'mxGeometry' in (item) and '@x' in item['mxGeometry']:
+            item.update({'@x':item['mxGeometry']['@x']})
+        objects_drawio_without_special_caract.append({k.replace('@', ''): item[k] for k in item})
+    return objects_drawio_without_special_caract
 
 
 if __name__ == '__main__':
     result = readxml("test-drawio-V5")
     my_dict = xmltodict.parse(result)
     objects_drawio = my_dict['mxfile']['diagram']['mxGraphModel']['root']['mxCell']
-    objects_drawio_without_special_caract = []
-    for item in objects_drawio:
-        if 'mxGeometry' in (item) and '@x' in item['mxGeometry']:
-            item.update({'@x':item['mxGeometry']['@x']})
-        objects_drawio_without_special_caract.append({k.replace('@', ''): item[k] for k in item})
-    result_nodes = [Node(**extraction) for extraction in objects_drawio_without_special_caract if len(extraction)>2]
-    endpoints = []
-    final_nodes = [item.value for item in result_nodes if item.value.find('eth'.lower()) != 0 and len(item.value)>0]
-    # search only the link between nodes
-    for node in result_nodes:
-        if node.value == '':
-            endpoint_id = node.id
-            endpoint_source = find_value(node.source)
-            endpoint_target = find_value(node.target)
-            endpoints.append([endpoint_id, endpoint_source[0], endpoint_target[0]])
-    links_endpoints = []
-    # search the interface used for the link
-    for node_id in endpoints:
-        interfaces = [[item.x, item.value] for item in result_nodes if item.value.find('eth') == 0 and item.parent == node_id[0]]
-        links_endpoints.append([f"{node_id[1]}:{interfaces[0][1]}, {node_id[2]}:{interfaces[1][1]}"])
-    # print(links_endpoints)
-    # JSON structure
-    final_config_JSON = {}
-    final_config_JSON.update({"name": "testLab"})
-    final_config_JSON.update({"topology": {"kinds": {"ceos": {"image": "arista/ceos:4.29.0.2F"}},"nodes":{}, "links":{}}})
-    for node in final_nodes:
-        final_config_JSON["topology"]["nodes"].update({node:{"kind": "ceos"}})
-    links = [{"endpoints": [item[0].split(",")[0],item[0].split(",")[1].strip()]} for item in links_endpoints]
-    final_config_JSON["topology"].update({"links": links})
 
-    pprint.pprint(final_config_JSON, indent= 4)
+    result = moving_x(objects_drawio)
+    objects_drawio_in_class_node = [Node(**extraction) for extraction in result if len(extraction) > 2]
+    nodes_yaml = [item.value for item in objects_drawio_in_class_node if item.value.lower().find('eth') != 0 and len(item.value) > 0]
+    links = find_link(objects_drawio_in_class_node)
+    links_yaml = find_interfaces(links)
+
+    lab_name = "Lab_test"
+    lab_image = "arista/ceos:4.29.0.2F"
+
+    file_loader = FileSystemLoader('templates')
+    env = Environment(loader=file_loader)
+    template = env.get_template('lab_template.txt')
+
+    output = template.render(lab_name=lab_name,lab_image=lab_image,nodes_yaml=nodes_yaml,links_yaml=links_yaml)
 
     with open('data/lab.yml', 'w') as yaml_file:
-        yaml.dump(final_config_JSON, yaml_file)
-    #
-    with open('data/exampleYAML.yml', 'r') as file:
-        configuration = yaml.safe_load(file)
-    pprint.pprint(configuration, indent= 4)
-
+        yaml_file.write(output)
